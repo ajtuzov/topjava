f91d87428352cc282af614b733ed7d80c2b56518
Andreu Tuzov <andreu.tuzov@gmail.com>
2020-10-18 21:59:20 +0300

    Main assignment has been implemented

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,4 +1,5 @@
 DELETE FROM user_roles;
+DELETE FROM meals;
 DELETE FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
@@ -9,3 +10,10 @@ VALUES ('User', 'user@yandex.ru', 'password'),
 INSERT INTO user_roles (role, user_id)
 VALUES ('USER', 100000),
        ('ADMIN', 100001);
+
+INSERT INTO meals (date_time, description, calories, user_id)
+VALUES ('2020-01-30 10:00:00', 'User: Завтрак', 500, 100000),
+       ('2020-01-29 01:00:00', 'User: Ночной жор', 2100, 100000),
+       ('2020-01-31 00:00:00', 'Admin: Передание', 2500, 100001),
+       ('2020-01-30 22:00:00', 'Admin: Сыр', 600, 100001),
+       ('2020-01-30 22:01:00', 'Admin: Вино', 400, 100001);
